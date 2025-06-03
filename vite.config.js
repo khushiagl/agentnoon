fa494524092079ae50c245d78f60f1594f47ec84
Khushi Agrawal <98438613+khushiagl@users.noreply.github.com>
2025-06-03 01:02:13 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [vue()],
+
   server: {
     // allow _any_ Host header (disables DNS-rebind protection)
     allowedHosts: true,
